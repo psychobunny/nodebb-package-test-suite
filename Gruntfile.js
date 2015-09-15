dadd4bee51d8e991331f8d067c46692357a2c4d2
psychobunny <rodrigues.andrew@gmail.com>
2015-09-15 19:51:00 -0400

    clone nodebb

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			server: {
-				files: ['lib/**/*.js', 'index.js'],
+				files: ['./lib/**/*.js', 'index.js'],
 				tasks: ['execute'],
 				options: {
 					livereload: false,
